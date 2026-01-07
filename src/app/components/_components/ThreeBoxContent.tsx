ac569b92992739b7269cd9ac345f95adc270a335
etranger2268 <shun.5.2268@gmail.com>
2026-01-07 22:21:28 +0900

    develop-02-08

---
+++ src/app/components/_components/ThreeBoxContent.tsx
@@ -0,0 +1,30 @@
+export default function ThreeBoxContent() {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <div className="shadow-xl p-6">
+        <h3 className="text-xl font-bold">これはタイトルです。</h3>
+        <p className="mt-4 text-base">
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+        </p>
+      </div>
+      <div className="shadow-xl p-6">
+        <h3 className="text-xl font-bold">これはタイトルです。</h3>
+        <p className="mt-4 text-base">
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+        </p>
+      </div>
+      <div className="shadow-xl p-6">
+        <h3 className="text-xl font-bold">これはタイトルです。</h3>
+        <p className="mt-4 text-base">
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+          これはコンテンツです。これはコンテンツです。これはコンテンツです。
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,32 +1,9 @@
+import ThreeBoxContent from '@/app/components/_components/ThreeBoxContent';
+
 export default function ComponentsPage() {
   return (
     <div className="max-w-4xl mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        <div className="shadow-xl p-6">
-          <h3 className="text-xl font-bold">これはタイトルです。</h3>
-          <p className="mt-4 text-base">
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-          </p>
-        </div>
-        <div className="shadow-xl p-6">
-          <h3 className="text-xl font-bold">これはタイトルです。</h3>
-          <p className="mt-4 text-base">
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-          </p>
-        </div>
-        <div className="shadow-xl p-6">
-          <h3 className="text-xl font-bold">これはタイトルです。</h3>
-          <p className="mt-4 text-base">
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-            これはコンテンツです。これはコンテンツです。これはコンテンツです。
-          </p>
-        </div>
-      </div>
+      <ThreeBoxContent />
     </div>
   );
 }
